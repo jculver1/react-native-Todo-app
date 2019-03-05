ea4d1540819dbe50769be59429c7dd4044f76f31
jculver1 <jculver1900@gmail.com>
2019-03-04 20:45:22 -0700

    first commit

---
+++ components/Main.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {
+  TextInput,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+
+export default class Main extends React.Component {
+  render(){
+    return (
+      <View style={StyleSheet.container}>
+      hello
+      </View>
+    )
+  }
+}
